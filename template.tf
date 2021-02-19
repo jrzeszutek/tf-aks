12d5433cbbb54ccbd9c5b9744cf378cd76038f20
jrzeszutek <jaroslaw@cloudify.co>
2021-02-19 11:46:26 +0100

    changed env variables reference in template

--- template.tf
+++ template.tf
@@ -24,6 +24,14 @@ variable "aks_name" {
   description = "AKS's name"
 }
 
+variable "ARM_CLIENT_ID" {
+  type = string
+}
+
+variable "ARM_CLIENT_SECRET" {
+  type = string
+}
+
 provider "azurerm" {
   features {}
 }
@@ -42,8 +50,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
   }
 
   service_principal {
-    client_id     = ${env.ARM_CLIENT_ID}
-    client_secret = ${env.ARM_CLIENT_SECRET}
+    client_id     = var.ARM_CLIENT_ID
+    client_secret = var.ARM_CLIENT_SECRET
   }
 
   role_based_access_control {
